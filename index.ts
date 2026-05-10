6789174e29790b47b4aadfc7f46a49f591e63d02
Kellerrel <kellerken.kk@gmail.com>
2026-05-10 14:43:29 +0300

    Add simple scraper

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import * as cheerio from 'cheerio';
+
+//Comics wepage Wumo
+//Please wait fetch
+let res = await fetch('https://wumo.com/wumo/2026/05/10');
+let html = await res.text();
+
+const $ = cheerio.load(html);
+//find image. Always first image.
+let img = $('img').first();
+console.log(img.attr('src')); //location
+console.log(img.attr('alt')); //date of the comics
