529ca3e0214144c49acdbef8713bddbb6b72cabf
Kellerrel <kellerken.kk@gmail.com>
2026-05-10 15:15:29 +0300

    Add Wumo scraper

--- index.ts
+++ index.ts
@@ -1,12 +1,35 @@
+import fs from 'fs';
 import * as cheerio from 'cheerio';
+
+// Source - https://stackoverflow.com/a/39914235
+// Posted by Dan Dascalescu, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-05-10, License - CC BY-SA 4.0
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+if (!fs.existsSync('./cache')) {
+    fs.mkdirSync('./cache');
+}
+
 
 //Comics wepage Wumo
 //Please wait fetch
-let res = await fetch('https://wumo.com/wumo/2026/05/10');
-let html = await res.text();
+for (let i = 1; i < 10; i++) { //add different day comics webpages
+    let html = '';
 
-const $ = cheerio.load(html);
-//find image. Always first image.
-let img = $('img').first();
-console.log(img.attr('src')); //location
-console.log(img.attr('alt')); //date of the comics
+    if (fs.existsSync(`./cache/${i}.html`)) {
+        html = fs.readFileSync(`./cache/${i}.html`, { encoding: 'utf-8' }); //read file using utf-8
+    }
+    else {
+        sleep(1000); //1sec delay
+        let res = await fetch(`https://wumo.com/wumo/2026/05/0${i}`);
+        html = await res.text();
+        fs.writeFileSync(`./cache/${i}.html`, html);
+    }
+
+    const $ = cheerio.load(html);
+    //find image. Always first image.
+    let img = $('img').first();
+    console.log(img.attr('src')); //img location url
+    console.log(img.attr('alt')); //date of the comics
+}
